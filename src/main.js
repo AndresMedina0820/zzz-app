e3d6e86f33b93f73b5080cf5d19eff8539ea55c9
Andres Medina <andres.medina@rocketfy.co>
2024-02-17 19:23:37 -0500

    Init project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import router from './routes';
+import Home from './pages/Home';
+
+// window.addEventListener('load', router);
+// window.addEventListener('hashchange', router);
+
+// const mainHtml = document.getElementById("app");
+// mainHtml.innerHTML = Home();
